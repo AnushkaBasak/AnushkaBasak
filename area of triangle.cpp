eb3a38726ccfa9bf47aa30327c1250f6b3140f7a
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-14 14:48:00 +0530

    Add files via upload

---
+++ area of triangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	float b,h,area;
+	printf("enter base and height of the triangle");
+	scanf("%f%f",&b,&h);
+	area=(1.0/2.0)*b*h;
+	printf("area of triangle is%f",area);
+}
+	
+
+
