d532837c0ce233484bdfc8d458ef12ba0d5aa9bf
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-14 14:43:45 +0530

    Add files via upload

---
+++ max. of 2 nos. (switch case).cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ main()
+{
+	int a,b;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	switch(a>b)
+	{
+		case 1:
+	    printf("First no. is maximum %d",a);
+		break;
+		
+		
+			case 2:
+	    printf("Second no. is maximum %d",b);
+		break;
+		
+		
+	default:
+		printf("Second no. is maximum %d",b);
+		}
+	}
+	
+			
+			
+			
+			
+				
+
