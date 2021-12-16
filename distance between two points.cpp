eec2c4063966b6cba8ce3ab114cd9b542a6257f3
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-16 14:29:04 +0530

    Add files via upload

---
+++ distance between two points.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b,c;
+	printf("enter value of first number a");
+	scanf("%d",&a);
+	printf("enter value of second number b");
+		scanf("%d",&b);
+		c=b,b=a,a=c;
+		printf("current value of a is:%d",&a);
+			printf("current value of b is:%d",&b);
+		}
+		
+	
+	
+
+	
+	
+	
+
