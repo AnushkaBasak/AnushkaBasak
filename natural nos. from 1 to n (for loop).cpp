dec4f91b997c46a561bc75da9670115cddcebb57
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-16 14:26:35 +0530

    Add files via upload

---
+++ natural nos. from 1 to n (for loop).cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("Enter the value of 'n'");
+	scanf("%d",&n);
+	printf("The first n natural nos. are: \n");
+	for (int i=1;i<=n;i++)
+	{
+		printf("%d \n",i);
+	}
+}
+	
+
