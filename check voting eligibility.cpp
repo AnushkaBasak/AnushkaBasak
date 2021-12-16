a2f424dc0b11a0e8e448ecc4fbba279816da08b4
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-16 14:39:24 +0530

    Add files via upload

---
+++ check voting eligibility.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter an age");
+	scanf("%d",&a);
+	if (a>=18)
+	{
+		printf("He/she is eligible for voting");
+	}
+	else
+	{
+			printf("He/she is not eligible for voting");
+	}
+}
+
+
