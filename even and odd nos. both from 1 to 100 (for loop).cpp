42d19839f0cacba442e1dd19817d08434bda66c9
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-16 14:28:15 +0530

    Add files via upload

---
+++ even and odd nos. both from 1 to 100 (for loop).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+   printf("The even nos. from 1 to 100 are: \n");
+	for (int i=0;i<=100;i+=2)
+	{
+		printf("%d \n",i);
+	}
+	printf("The odd nos. from 1 to 100 are: \n");
+	for (int i=1;i<=100;i+=2)
+	{
+		printf("%d \n",i);
+	}
+	
+}
+
