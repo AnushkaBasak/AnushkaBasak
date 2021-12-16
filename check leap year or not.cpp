031fb459eee3ded1ef4a04bd4b14e5ce3c514423
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-16 14:40:48 +0530

    Add files via upload

---
+++ check leap year or not.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int year;
+	printf("enter any year");
+	scanf("%d",&year);
+	if (year%4==0) 
+	{
+		printf("it is a leap year");
+	}
+	else
+	{
+		printf("it is not a leap year");
+	}
+	
+}
+
