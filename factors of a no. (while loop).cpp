566342c2dcbaef7a6993c1a7c3aa3fa8a34351b7
AnushkaBasak <95929171+AnushkaBasak@users.noreply.github.com>
2021-12-23 14:32:33 +0530

    Add files via upload

---
+++ factors of a no. (while loop).cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{   
+    int n,i;
+    i=1;
+	printf("enter a number");
+	scanf("%d",&n);
+	printf("The factors are:\n");
+	while(i<=n)
+	{
+		if (n%i==0)
+	
+	
+			printf("%d\n",i);
+i++;
+	
+}
+}
+
+
